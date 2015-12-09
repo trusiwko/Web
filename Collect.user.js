7f6cf7b6ee11a0d52d665a6499ece8ce398c4b6e
Павел <feedback@usbo.info>
2015-12-09 09:46:31 +0400

    +raiffeisen +vtb24

--- Collect.user.js
+++ Collect.user.js
@@ -7,9 +7,12 @@
 // @match        https://mybank.oplata.kykyryza.ru/
 // @match        https://iclick.imoneybank.ru/*
 // @match        https://my.tinkoff.ru/*
+// @match        https://connect.raiffeisen.ru/rba/*
+// @match        https://online.vtb24.ru/content/telebank-client/ru/login/telebank/*
 // @updateURL    https://raw.githubusercontent.com/trusiwko/Web/master/Collect.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
@@ -17,7 +20,7 @@
 // GM_setValue( 'acc_need', false );
 // GM_setValue( 'secret', 'secret' );
 
-if (location.hostname != "iclick.imoneybank.ru") {
+if (location.hostname != "iclick.imoneybank.ru" && location.hostname != 'connect.raiffeisen.ru' && location.hostname != 'online.vtb24.ru') {
     var s = document.createElement("script");
     s.type = "text/javascript";
     s.src = "https://code.jquery.com/jquery-2.1.4.min.js";
@@ -44,7 +47,7 @@ function start() {
     usbo_secret = GM_getValue( 'secret', '-' );
     if (usbo_secret == '-') 
         alert('ВНИМАНИЕ! Необходимо установить секрет');
-    var div = jQuery('<div />').attr('id', 'usbo_btn').css('z-index', 2).css('position', 'fixed').css('top', 0).css('right', 0).css('padding', 10).css('padding-right', 20).appendTo($('body'));
+    var div = jQuery('<div />').attr('id', 'usbo_btn').css('z-index', 9001).css('position', 'fixed').css('top', 0).css('right', 10).css('padding', 10).appendTo($('body'));
     var btn = jQuery('<button />').html('+ usbo.info').appendTo(div).on('click', syncStart);
 }
 
@@ -81,13 +84,82 @@ function start_tinkoff() {
     }
 }
 
+function vtb1() {
+	var e = $('.js-head').data('id');
+	var h = 'statementRootElement';
+	var j = 'CompositeStatement';
+	var l = 'Csv';
+	var g = $('.productsStatement');
+	if (!e || !j) {
+		TBC.log("submitDownloadRequest skipped. id or classType not found. objectId: " + e + ", objectClassType: " + j);
+		return
+	}
+	var k = j + ":" + e + ":" + l;
+	function f(n) {
+		var m = n.errors[0].msg || TBC.I18n.get("label.products.failedTimeOutMessage");
+		g.hideLoader();
+		c.showInformationPopUp(m)
+	}
+	function d(m) {
+		if (topicUtils.hasError(m)) {
+			f(m)
+		} else {
+			g.hideLoader();
+			
+            var pd = {
+				cacheKey: 'CompositeStatement:' + $('.js-head').data('id') + ':Csv',
+				errorPage: "/content/telebank-client/" + TBC.I18n.getLocale() + "/error-pages/500.html",
+				pageToken: TBC.securityManager.getPageToken(),
+				format: 'Csv'		
+			};
+            
+            $.ajax({
+                method: "POST",
+                url: "/processor/process/minerva/document",
+                //contentType: 'Content-type: text/csv; charset=windows-1251', // Странно, но не работает тут
+                data: pd,
+                success: function(data) {
+                    //console.log(data);
+                    arr = data.split("\n");
+                    arr.reverse();
+                    next();
+                },
+                beforeSend: function(jqXHR) { jqXHR.overrideMimeType('text/csv;charset=windows-1251'); }
+            });
+
+		}
+	}
+	TBC.dataProvider.registerComponent({
+		id: h,
+		callback: d,
+		failedCallback: f
+	}, [{
+		id: "DOCUMENT_PRESENTATION",
+		execute: true,
+		getIncomeParams: {
+			cacheKey: k,
+			fileName: "statement"
+		},
+		params: {
+			objectId: e,
+			className: j,
+			format: l,
+			fileName: "details",
+			pageToken: TBC.securityManager.getPageToken()
+		}
+	}]);
+	g.showLoader()
+}
+
 function syncStart() {
     console.log('start');
     arr = new Array();
     res = new Array();
     nsend = 0;
     if (location.hostname == "mybank.oplata.kykyryza.ru") {
-        account = $('.slider_cards_card_shirt_inner_back_ean_first').html() + $('.slider_cards_card_shirt_inner_back_ean_second').html() + $('.slider_cards_card_shirt_inner_back_ean_third').html();
+        account = $('.card-list_card_name_text').text();
+        if (account == "") account = $('.slider_cards_card_info_card-name').text();
+        //$('.slider_cards_card_shirt_inner_back_ean_first').html() + $('.slider_cards_card_shirt_inner_back_ean_second').html() + $('.slider_cards_card_shirt_inner_back_ean_third').html();
         type = 'Кукуруза';
         $('.history_operations_day_operation').each(function(a, b) {
 
@@ -172,6 +244,34 @@ function syncStart() {
                 arr.push(o);
         });
         */
+    } else if (location.hostname == 'connect.raiffeisen.ru') {
+        type = 'Raiffeisen';
+        account = $('option[value="'+$('select[name="objectId"]').val()+'"]').text();
+        //getReport('showStatementForm', 'CSV')
+        var link = $('#showStatementForm').attr('action');
+        var data = {};
+        $('#showStatementForm').find('input').each(function(a,b) {
+            if (typeof $(b).attr('name') != 'undefined') {
+                data[$(b).attr('name')] = $(b).val();
+            }
+        });
+        $('#showStatementForm').find('select').each(function(a,b) {
+            if (typeof $(b).attr('name') != 'undefined') {
+                data[$(b).attr('name')] = $(b).val();
+            }
+        });
+        data['reportType'] = 'CSV';
+        //console.log(data);
+        $.post(link, data, function(data) {
+            arr = data.split("\n");
+            next();
+        });
+        return;
+    } else if (location.hostname == 'online.vtb24.ru') {
+        type = 'VTB24';
+        account = $('.customCheckbox.checked').parent().parent().find('.productInfo i span').text();
+        vtb1();
+        return;
     }
     if (!GM_getValue( 'acc_need', true )) {
         account = '-';
@@ -182,10 +282,28 @@ function syncStart() {
 function next() {
     var a = arr.splice(0, 50);
     if (a.length) {
-        $.post('https://usbo.info/collect/save/', {part: nsend, secret: usbo_secret, type: type, account: account, data: a}, function(data){
-            res.push(data);
-            next();
-        });
+        if (location.hostname == 'online.vtb24.ru') {
+            // $.param не работает с массивами в этой версии jQuery
+            var ks = "part=" + nsend + "&secret=" + usbo_secret + "&type=" + type + "&account=" + account;
+            for(i=0;i<a.length;i++) {ks += '&data[]=' + encodeURIComponent(a[i]);}
+            GM_xmlhttpRequest({
+                method: "POST",
+                url: 'https://usbo.info/collect/save/',
+                data: ks,
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                onload: function(data) {
+                    res.push(data.responseText);
+                    next();
+                }
+            });
+        } else {
+            $.post('https://usbo.info/collect/save/', {part: nsend, secret: usbo_secret, type: type, account: account, data: a}, function(data){
+                res.push(data);
+                next();
+            });
+        }
         nsend++;
     } else {
         alert(res.join("\n"));
