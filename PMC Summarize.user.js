79fd076df9e4c3ef457a7b725581e25a3ddb0bc6
trusiwko <trusiwka@gmail.com>
2016-12-01 15:07:05 +0400

    Update PMC Summarize.user.js

--- PMC Summarize.user.js
+++ PMC Summarize.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         PMC Summarize
-// @version      0.1
+// @version      0.2
 // @description  Get this sum!
 // @author       Pavel Goncharenko
 // @match        https://pmc.epam.com/pmc/timereport/list.do
@@ -9,22 +9,22 @@
 
 (function() {
     'use strict';
-
-    var allsum = 0;
-    $('tbody.listTableContentUnion').each(function(a,b) {
-        var body = $(b);
-        var head = $(b).prev();
-        var rfc = body.find('tr').eq(0).find('td').eq(2).find('a').text();
-        rfc = rfc.split(" ")[0];
-        head.find('a').html(head.find('a').html().replace(/Assignment ID:/, rfc + ': '));
-        var sum = 0;
-        body.find('tr').each(function(c,d) {
-            var n = $(d).find('td').eq(5).text();
-            sum += parseFloat(n);
+    if ($('select[name=groupBy]').val() == "assignmentId") {
+        var allsum = 0;
+        $('tbody.listTableContentUnion').each(function(a,b) {
+            var body = $(b);
+            var head = $(b).prev();
+            var rfc = body.find('tr').eq(0).find('td').eq(2).find('a').text();
+            rfc = rfc.split(" ")[0];
+            head.find('a').html(head.find('a').html().replace(/Assignment ID:/, rfc + ': ').replace(/ID Назначения:/, rfc + ': '));
+            var sum = 0;
+            body.find('tr').each(function(c,d) {
+                var n = $(d).find('td').eq(5).text();
+                sum += parseFloat(n);
+            });
+            allsum += sum;
+            head.find('.wdGroupBy').text(sum);
         });
-        allsum += sum;
-        head.find('.wdGroupBy').text(sum);
-    });
-    $('<div />').css('fontSize', '120%').css('padding', '10px').html('Итого: <strong>' + allsum + '</strong>').insertAfter($('.listTableContainer'));
-
+        $('<div />').css('fontSize', '120%').css('padding', '10px').html('Итого: <strong>' + allsum + '</strong>').insertAfter($('.listTableContainer'));
+    }
 })();
