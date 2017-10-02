d57c51cb89e124c1cafb9c5bbfc833c74b7a4a76
trusiwko <trusiwka@gmail.com>
2017-10-02 09:06:16 +0400

    Update TIME.EPAM.user.js

--- TIME.EPAM.user.js
+++ TIME.EPAM.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TIME.EPAM
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://time.epam.com/journal/4060741400007522479/*
